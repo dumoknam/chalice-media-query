fa8a1ac193a8d75f87d598f85b86dc2e9348ae78
dumok <nsh330@gmail.com>
2018-11-25 21:40:07 +0900

    Add s3 create event handler

--- media-query/app.py
+++ media-query/app.py
@@ -9,7 +9,10 @@ app = Chalice(app_name='media-query')
 
 _MEDIA_DB = None
 _REKOGNITION_CLIENT = None
-
+_SUPPORTED_IMAGE_EXTENSIONS = (
+    '.jpg',
+    '.png',
+)
 
 def get_media_db():
     global _MEDIA_DB
@@ -31,9 +34,16 @@ def get_rekognition_client():
         return _REKOGNITION_CLIENT
 
 
-@app.lambda_function()
-def detect_labels_on_image(event, context):
-    bucket = event['Bucket']
-    key = event['Key']
+@app.on_s3_event(bucket=os.environ['MEDIA_BUCKET_NAME'], events=['s3:ObjectCreated:*'])
+def handle_object_created(event):
+    if _is_image(event.key):
+        _handle_created_image(bucket=event.bucket, key=event.key)
+
+
+def _is_image(key):
+    return key.endswith(_SUPPORTED_IMAGE_EXTENSIONS)
+
+
+def _handle_created_image(bucket, key):
     labels = get_rekognition_client().get_image_labels(bucket=bucket, key=key)
     get_media_db().add_media_file(key, media_type=db.IMAGE_TYPE, labels=labels)
